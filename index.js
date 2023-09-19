f73f98edef29e7d326ac44a2f11b9831404070a7
expenile <129822353+expenile@users.noreply.github.com>
2023-09-19 23:40:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let countEl = document.getElementById("count-el");
+console.log(countEl);
+let count =0;
+
+function increment(){
+    count = count + 1;
+    countEl.innerText=count;
+    
+}
+function decrement(){
+    count = count - 1;
+    countEl.innerText=count;
+    
+}
+let saveEl = document.getElementById("save-el");
+function save(){
+    let countStr =" " + count + " → ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
+
+  
+
+  
